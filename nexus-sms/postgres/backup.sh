1c3d1ee5cc0a351beee921bc4d35454a6d1af9e5
Luciano Mattos <luciano@mattos.eti.br>
2023-04-24 12:32:09 -0300

    .

---
+++ nexus-sms/postgres/backup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+database_name='nexus_api3'
+
+backup_name='/home/postgres/backup/POSTGRESS_'$database_name'_'$(date '+%Y%m%d%H%M')'.BKP'
+
+pg_dump \
+ -h localhost \
+ -U postgres \
+ -Fc \
+ -p 5432 \
+ -Z5 \
+ -f $backup_name $database_name
+ 
