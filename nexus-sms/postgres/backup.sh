5bb618680a1736b1cac9f516af4b4547f0549017
Luciano Mattos <luciano@mattos.eti.br>
2023-04-26 11:16:57 -0300

    .

--- nexus-sms/postgres/backup.sh
+++ nexus-sms/postgres/backup.sh
@@ -3,6 +3,8 @@
 # Incluir a linha comando do crontab
 # 00 23 * * * /home/nexboxprod/Postgres/Script/backup.sh
 
+# sudo mount -t cifs //192.168.15.18/NetBackup/Postgres/whatsapp_prod /home/nexboxprod/Postgres/Backup -o username=backup,password=8&u(yC9l,uid=1000,gid=1000
+
 database_name='whatsapp_prod'
 
 backup_name='/home/nexboxprod/Postgres/Backup/'$database_name'_'$(date '+%Y%m%d%H%M')'.bkp'
